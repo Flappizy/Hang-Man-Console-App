0731ab9525018bdef2eded146e31c0602d750593
flappizy <babatundeibukun52@gmail.com>
2021-01-11 04:46:55 +0100

    First commit of hang man console game

---
+++ TestProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HangManApp
+{
+    class TestProgram
+    {
+        static void Main()
+        {
+            int player = GamePlayer.ChooseGamePlayer();
+            string wordToBeGuessed = string.Empty;
+            switch (player)
+            {
+                case 0:
+                    wordToBeGuessed = GamePlayerComputer.PlayWithComputer();
+                    break;
+                case 1:
+                    wordToBeGuessed = HumanGamePlayer.PlayWithHuman();
+                    break;
+            }
+
+            GamePlay.PlayGame(wordToBeGuessed);
+        }
+    }
+}
